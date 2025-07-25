1fb9258d5b30f51c044fe154c8ecbea3ab8866a5
Tristan Kitto <kittr801@student.otago.ac.nz>
2025-07-26 01:49:34 +1200

    Implemented inventory system to store collected game objects

--- Assets/Scripts/ItemScript.cs
+++ Assets/Scripts/ItemScript.cs
@@ -18,7 +18,19 @@ public class ItemScript : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Item collision detected with: " + other.gameObject.name);
-        Destroy(gameObject);
+        if (other.CompareTag("Player"))
+        {
+            Inventory inventory = other.GetComponent<Inventory>();
+            if (inventory != null)
+            {
+                Debug.Log("Item collision detected with: " + other.gameObject.name);
+                inventory.AddItem(gameObject);
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.Log("Item collision with non-player object: " + other.gameObject.name);
+        }
     }
 }
