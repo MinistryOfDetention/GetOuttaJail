be7fe1ed2f9610159ac31a474893336975f7ee0b
Tristan Kitto <kittr801@student.otago.ac.nz>
2025-07-26 14:10:29 +1200

    Scene restarts when teacher collides with player

--- Assets/Scripts/TeacherScript.cs
+++ Assets/Scripts/TeacherScript.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using NavMeshPlus.Extensions;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TeacherScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public GameObject target;
-    
+
     private UnityEngine.AI.NavMeshAgent agent;
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-		agent.updateRotation = false;
-		agent.updateUpAxis = false;
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
@@ -22,9 +23,16 @@ public class TeacherScript : MonoBehaviour
     {
         if (agent.path.corners.Length > 1)
         {
-            //Debug.Log(agent.path.corners[1] - agent.path.corners[0]);
             Debug.Log(agent.path.corners[1]);
         }
         agent.SetDestination(target.transform.position);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
