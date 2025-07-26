4e6133799a1b36727e25c19770181f8338590cad
62firelight <lktfirelight@gmail.com>
2025-07-26 14:02:10 +1200

    Rotate enemy towards player when being chased

--- Assets/Scripts/TeacherScript.cs
+++ Assets/Scripts/TeacherScript.cs
@@ -8,13 +8,15 @@ public class TeacherScript : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject target;
-    
+
     private UnityEngine.AI.NavMeshAgent agent;
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-		agent.updateRotation = false;
-		agent.updateUpAxis = false;
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        // Wait to be given a target (e.g., by the EnemyVision script)
     }
 
     // Update is called once per frame
@@ -25,6 +27,15 @@ public class TeacherScript : MonoBehaviour
             //Debug.Log(agent.path.corners[1] - agent.path.corners[0]);
             Debug.Log(agent.path.corners[1]);
         }
-        agent.SetDestination(target.transform.position);
+
+        if (target)
+        {
+            // Face the target (most likely the player)
+            Vector2 targ = new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y);
+            float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            agent.SetDestination(target.transform.position);
+        }
     }
 }
