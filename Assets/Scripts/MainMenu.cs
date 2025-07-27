b326187330ed8441cabd9a4dad09515fddc722a8
Luke Webb <lukewebb2000@gmail.com>
2025-07-27 22:10:35 +1200

    Main Menu changes, added credits

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
 
+    public RectTransform CreditPanel;
     public void StartGame()
     {
         // Load the game scene
@@ -15,5 +17,17 @@ public class MainMenu : MonoBehaviour
     {
         // Quit the application
         Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+        // Show the credits panel
+        CreditPanel.gameObject.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        // Hide the credits panel
+        CreditPanel.gameObject.SetActive(false);
     }
 }
