2d8eeb1368cec8cf02e69c7ef3db352c0c0cc1f6
62firelight <lktfirelight@gmail.com>
2025-07-27 21:56:24 +1200

    Add pen tutorial which happens only once

--- Assets/Scripts/ItemScript.cs
+++ Assets/Scripts/ItemScript.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class ItemScript : MonoBehaviour
 {
+    public DialogueManager dm;
+
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        dm = GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueManager>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -26,6 +22,16 @@ public class ItemScript : MonoBehaviour
                 //Debug.Log("Item collision detected with: " + other.gameObject.name);
                 inventory.AddItem(gameObject);
                 gameObject.SetActive(false);
+
+                if (!LevelMaster.penTutorialStarted)
+                {
+                    dm.AddDialogue(new string[] {
+                        "A pen? I can use the SHIFT key to launch it at teachers!",
+                        "I'll need to drop the pen when I'm entering a new room though..."
+                    });
+                    LevelMaster.penTutorialStarted = true;
+                }
+
             }
         }
         else
