97be41b00fd2d51089d3ad68ff8552c99c595095
magicalmanjack <magicalmanjack@gmail.com>
2025-07-27 16:07:28 +1200

    Added question mark animation above teacher

--- Assets/Scripts/TeacherScript.cs
+++ Assets/Scripts/TeacherScript.cs
@@ -20,6 +20,9 @@ public class TeacherScript : MonoBehaviour
     public float stunTime;
 
     public float speed;
+
+    public GameObject stunIconPrefab;
+    private GameObject stunIcon;
     private UnityEngine.AI.NavMeshAgent agent;
     private Vector3 nextDest;
     private bool playerDetected = false;
@@ -36,12 +39,12 @@ public class TeacherScript : MonoBehaviour
     private Boolean Stunned = false;
 
     void Start()
-    {   
+    {
         animator = GetComponentInChildren<Animator>();
         if (animator == null)
         {
             Debug.LogError("Animator component not found on TeacherScript.");
-        } 
+        }
         visionCone = transform.GetChild(0);
         nextDest = transform.position;
 
@@ -49,6 +52,7 @@ public class TeacherScript : MonoBehaviour
         {
             target = patrolWaypoints[currentPatrolWaypointIndex].gameObject;
         }
+
     }
 
     class Node
@@ -257,15 +261,20 @@ public class TeacherScript : MonoBehaviour
         }
 
         Stunned = false;
+        Destroy(stunIcon);
     }
     void Stun()
     {
         // Activates the stun phase of the teacher.
         Stunned = true;
+
         if (StunCoroutine != null)
         {
             StopCoroutine(StunCoroutine);
+            Destroy(stunIcon);
         }
+
+        stunIcon = Instantiate(stunIconPrefab, transform.position + Vector3.up * 2.5f, Quaternion.identity);
 
         StunCoroutine = StunIEnumerator();
 
